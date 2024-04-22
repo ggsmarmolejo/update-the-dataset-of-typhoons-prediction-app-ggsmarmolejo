3a31fe3f5824c02f1d4320c2ad9a0b6498776808
Glaiza Marmolejo <99538281+ggsmarmolejo@users.noreply.github.com>
2024-04-22 10:03:32 +0800

    Update PH_Typhoons.py

--- PH_Typhoons.py
+++ PH_Typhoons.py
@@ -2,7 +2,6 @@ import streamlit as st
 import tensorflow as tf
 import numpy as np
 import pandas as pd
-from sklearn.preprocessing import MinMaxScaler
 import matplotlib.pyplot as plt
 import time
 
